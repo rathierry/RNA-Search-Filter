91578bd9d2623a97a09ee82a6f8a92f9e13435f6
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-04-13 02:36:20 +0200

    ✨ (App) fetch data from API

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const TIMEOUT = 3000; // 3 seconds
+const ITEMS_PER_PAGE = 100;
+const PAGE = 1;
+const BASE_URL = 'https://randomuser.me';
+const API_URL = `${BASE_URL}/api?results=${ITEMS_PER_PAGE}&page=${PAGE}`;
 
 export default App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
 
+  const init = () => {
+    console.log("init init");
+    setIsLoading(true);
+    setTimeout(() => {
+      fetchData(API_URL);
+    }, TIMEOUT);};
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      const results = json.results;
+      setData(results);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+  };
+
   const handleSearch = (query) => {
-    console.log("🚀 ~ file: App.js:8 ~ handleSearch ~ query:", query)
     setSearchQuery(query);
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size='large' color="#89CFF0" />
+        <Text>Loading ...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text>Error on fetching data ...</Text>
+        <Text>Please check your internet connection!</Text>
+        <Pressable onPress={init} style={styles.button}>
+          <Text>Refresh</Text>
+        </Pressable>
+      </View>
+    );
   }
 
   return (
@@ -29,6 +82,23 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 20,
     backgroundColor: '#FFF',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#89CFF0',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#CCC',
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    marginTop: 6,
+    elevation: 8
   },
   searchBox: {
     paddingHorizontal: 20,
